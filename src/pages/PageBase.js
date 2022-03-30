df10dbdf5b7e676901929e535e408e70f199697b
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-03-30 19:24:39 +0500

    created burger menu

--- src/pages/PageBase.js
+++ src/pages/PageBase.js
@@ -1,8 +1,10 @@
 import { Header,Footer } from "../components"
+import { BurgerMenu } from "../components/BurgerMenu/BurgerMenu"
 
 export function PageBase(props) {
     return (
         <div className="App">
+            
             <Header />
             {props.children}
             <Footer />
