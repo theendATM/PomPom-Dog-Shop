267ae7acd25684dea4482fbd9595ba7acb4a6b9b
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-03-31 13:54:37 +0500

    added login screen

--- src/pages/PageBase.js
+++ src/pages/PageBase.js
@@ -1,8 +1,17 @@
-import { Header,Footer } from "../components"
+import { useSelector } from "react-redux"
+import { Header, Footer } from "../components"
+import AuthorizationLogIn from "../components/Authorization/AuthorizationLogIn"
+import AuthorizationSignUp from "../components/Authorization/AuthorizationSignUp"
 
 export function PageBase(props) {
+
+    const isLogging=useSelector(s=>s.login)
+    const isSigningUp=useSelector(s=>s.signup)    
+
     return (
         <div className="App">
+            <AuthorizationLogIn visible={isLogging}/>
+            <AuthorizationSignUp visible={isSigningUp}/>
             <Header />
             {props.children}
             <Footer />
--- src/storage/PomPomStorage.js
+++ src/storage/PomPomStorage.js
@@ -1,20 +1,25 @@
 import { createStore, Reducer, Action } from "redux";
 import { BreedTypes } from "../API/dogapi";
+import { combineReducers } from "redux";
 
 const initState = {
-    query: "all"
+    query: "all",
+    login: false,
+    signup: false
 }
 
-const queryReducer = (state=initState , action) => {
+const queryReducer = (state = initState, action) => {
 
-    switch(action.type){
-        case "CHANGE": return { query: BreedTypes[action.payload - 1] }
-        default:return initState
+    switch (action.type) {
+        case "CHANGE": return { query: BreedTypes[action.payload - 1], login: state.login, signup: state.signup }
+        case "LOGIN": return { query: state.query, login: true, signup: false }
+        case "SIGNUP": return { query: state.query, login: false, signup: true }
+        default: return initState
     }
 
-   
+
 
 
 }
 
-export const storage = createStore(queryReducer,initState)
+export const storage = createStore(queryReducer, initState)
