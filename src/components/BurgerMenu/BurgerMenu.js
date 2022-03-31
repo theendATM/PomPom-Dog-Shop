ee6a58c7fa6249bd018e25d0e8ba0f73c3bb6b45
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-03-31 10:12:41 +0500

    fixed className

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -3,17 +3,20 @@ import Cross from "../Authorization/Vector.svg"
 import { createPortal } from "react-dom"
 
 export function BurgerMenu(props) {
-    return (
+    if (!props.open)
+        return null;
 
-        <div id="shade">
-            <div id="burger-menu">
-                <img id="cross" src={Cross} />
-                <div id="buttons-container">
-                    <button className="burger-button">Главная</button>
-                    <button className="burger-button">Все питомцы</button>
-                    <button className="burger-button">О нас</button>
-                    <button className="burger-button">Войти или зарегистрироваться</button>
+    return (
+        createPortal(
+            <div id="shade">
+                <div id="burger-menu">
+                    <img id="cross" src={Cross} />
+                    <div id="buttons-container">
+                        <button className="burger-button">Главная</button>
+                        <button className="burger-button">Все питомцы</button>
+                        <button className="burger-button">О нас</button>
+                        <button className="burger-button">Войти или зарегистрироваться</button>
+                    </div>
                 </div>
-            </div>
-        </div>)
+            </div>), document.getElementById("portal"))
 }
--- src/pages/PageBase.js
+++ src/pages/PageBase.js
@@ -4,7 +4,7 @@ import { BurgerMenu } from "../components/BurgerMenu/BurgerMenu"
 export function PageBase(props) {
     return (
         <div className="App">
-            
+            <BurgerMenu open={false}></BurgerMenu>
             <Header />
             {props.children}
             <Footer />
