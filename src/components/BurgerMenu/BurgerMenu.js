1f8ad9722cde70a5b435381e80bfd46f7a2e78eb
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-04-01 17:22:49 +0500

    finished burger menu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,17 +1,40 @@
 import { createPortal } from "react-dom"
 import "./burgerMenu.css"
+import Cross from "../Authorization/Vector.svg"
+import { storage } from "../../storage/PomPomStorage"
+import {useNavigate} from 'react-router-dom'
+import { btnStateStorage } from "../../storage/PageState";
+
+function close(){
+    storage.dispatch({type:"CLOSEMENU"})
+}
 
 export function BurgerMenu(props) {
 
+    const navigate = useNavigate()
+    const btnStates = btnStateStorage.getState()
+
+    const changeNavStyle = (btnType) => {
+        btnStateStorage.dispatch({type:btnType})
+    }
+
     const portal=document.getElementById("portal")
-    if (!props.visible||portal==null)
+    if (!props.visible || portal == null)
         return null;
 
     return (
         createPortal(
 
             <div id="shade">
-
+                <div id="pane">
+                    <img id="cross" src={Cross} onClick={close}/>
+                    <div id="buttons-container">
+                        <button onClick={()=>{changeNavStyle("MAIN_PAGE"); navigate("/");close()}} isActive={btnStates.main}>Главная</button>
+                        <button onClick={()=>{changeNavStyle("ALLPETS_PAGE"); navigate("/all");close()}} isActive={btnStates.allPets}>Все питомцы</button>
+                        <button onClick={()=>{changeNavStyle("ABOUT_PAGE"); navigate("/about");close()}} isActive={btnStates.about}>О нас</button>
+                        <button>Войти или зарегистрироваться</button>
+                    </div>
+                </div>
             </div>,
 
             portal)
