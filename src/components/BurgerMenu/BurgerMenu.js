9bdae7baae6398c2816d7f723b1eed89b6aac4e7
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-04-01 18:15:45 +0500

    Update BurgerMenu.js

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -2,11 +2,11 @@ import { createPortal } from "react-dom"
 import "./burgerMenu.css"
 import Cross from "../Authorization/Vector.svg"
 import { storage } from "../../storage/PomPomStorage"
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { btnStateStorage } from "../../storage/PageState";
 
-function close(){
-    storage.dispatch({type:"CLOSEMENU"})
+function close() {
+    storage.dispatch({ type: "CLOSEMENU" })
 }
 
 export function BurgerMenu(props) {
@@ -15,10 +15,10 @@ export function BurgerMenu(props) {
     const btnStates = btnStateStorage.getState()
 
     const changeNavStyle = (btnType) => {
-        btnStateStorage.dispatch({type:btnType})
+        btnStateStorage.dispatch({ type: btnType })
     }
 
-    const portal=document.getElementById("portal")
+    const portal = document.getElementById("portal")
     if (!props.visible || portal == null)
         return null;
 
@@ -27,11 +27,11 @@ export function BurgerMenu(props) {
 
             <div id="shade">
                 <div id="pane">
-                    <img id="cross" src={Cross} onClick={close}/>
+                    <img id="cross" src={Cross} onClick={close} />
                     <div id="buttons-container">
-                        <button onClick={()=>{changeNavStyle("MAIN_PAGE"); navigate("/");close()}} isActive={btnStates.main}>Главная</button>
-                        <button onClick={()=>{changeNavStyle("ALLPETS_PAGE"); navigate("/all");close()}} isActive={btnStates.allPets}>Все питомцы</button>
-                        <button onClick={()=>{changeNavStyle("ABOUT_PAGE"); navigate("/about");close()}} isActive={btnStates.about}>О нас</button>
+                        <button onClick={() => { changeNavStyle("MAIN_PAGE"); navigate("/"); close() }}>Главная</button>
+                        <button onClick={() => { changeNavStyle("ALLPETS_PAGE"); navigate("/all"); close() }}>Все питомцы</button>
+                        <button onClick={() => { changeNavStyle("ABOUT_PAGE"); navigate("/about"); close() }} >О нас</button>
                         <button>Войти или зарегистрироваться</button>
                     </div>
                 </div>
